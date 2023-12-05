fb82f6ca337a73233c7cb3a2d9a8fc311fc6e4e0
Avadhut <avadhutl@celcomsolutions.com>
2023-12-05 15:29:51 +0530

    function samples added

---
+++ core-java-demo/src/com/kodtodya/practice/functions/FunctionDemo.java
@@ -0,0 +1,32 @@
+package com.kodtodya.practice.functions;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class FunctionDemo {
+
+    // for non-static method calls using function
+    private NumberPlayground numberPlayground = new NumberPlayground();
+
+    private Function<Integer, Boolean> isArmstrong = numberPlayground::isArmstrong;
+
+    // for static method calls using function
+    private Consumer<Integer> checkNumber = NumberPlayground::checkNumber;
+
+    public static void main(String[] args) {
+        FunctionDemo functionDemo = new FunctionDemo();
+
+        // local non-static method call
+        functionDemo.runPlaygroundCheck();
+    }
+
+    private void runPlaygroundCheck() {
+        // usage of non-static method calls using function
+        System.out.println(this.isArmstrong.apply(153) ? "Armstrong" : "Not Armstrong");
+
+        // usage of static method calls using function
+        checkNumber.accept(121);
+    }
+}
