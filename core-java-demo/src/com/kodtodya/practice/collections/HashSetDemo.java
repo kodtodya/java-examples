d11df0f19f817d09c6866e11a2f2615b66c04b1a
Avadhut <kodtodya@gmail.com>
2024-01-07 14:40:47 +0530

    collections

--- core-java-demo/src/com/kodtodya/practice/collections/HashSetDemo.java
+++ core-java-demo/src/com/kodtodya/practice/collections/HashSetDemo.java
@@ -1,30 +1,80 @@
 package com.kodtodya.practice.collections;
 
-import java.util.Set;
-import java.util.HashSet;
+import java.util.*;
 
 class HashSetDemo {
 
     public static void main(String[] args) {
-        // Creating a set using the HashSet class
-        Set<Integer> set1 = new HashSet<>();
+        // Creating set using the HashSet class
+        Set<String> cities = new HashSet<>();
 
-        // Add elements to the set1
-        set1.add(2);
-        set1.add(3);
-        System.out.println("Set1: " + set1);
+        // add() - Add elements to the set
+        cities.add("Pune"); // index = 0
+        cities.add("Mumbai"); // index = 1
+        cities.add("Delhi"); // index = 2
+        System.out.println("Set: " + cities);
+        System.out.println("-----------------------------");
 
-        // Creating another set using the HashSet class
-        Set<Integer> set2 = new HashSet<>();
+        //remove() - removes an element from the set
+        System.out.println("Removed Element- \"Mumbai\": " + cities.remove("Mumbai"));
+        System.out.println("Remaining set: " + cities);
+        System.out.println("-----------------------------");
 
-        //add() - adds the specified element to the set
-        set2.add(1);
-        set2.add(2);
-        System.out.println("Set2: " + set2);
+        Set<String> newCitiesSet = new HashSet<>();
+        //addAll() - adds all elements of one set to another
+        newCitiesSet.addAll(cities);
+        newCitiesSet.add("Bengaluru");
+        System.out.println("New Cities Set: " + newCitiesSet);
+        System.out.println("-----------------------------");
 
-        //addAll() - adds all the elements of the specified collection to the set - Union of two sets
-        set2.addAll(set1);
-        System.out.println("Union is: " + set2);
+        //contains() - returns true if a set contains specified element
+        if (newCitiesSet.contains(2)) {
+            System.out.println(2 + " value is present in newCitiesSet");
+        }
+
+        if (newCitiesSet.contains("Bengaluru")) {
+            System.out.println("\"Bengaluru\" value is present in newCitiesSet");
+        }
+        System.out.println("-----------------------------");
+
+        //size() - returns the length of set
+        System.out.println("Size of newCitiesSet: " + newCitiesSet.size());
+        System.out.println("-----------------------------");
+
+        //iterator() - returns iterator object that can be used to sequentially access elements of set
+        Iterator<String> citiesIterator = newCitiesSet.iterator();
+        while (citiesIterator.hasNext()) {
+            System.out.println("next element in newCitiesSet: " + citiesIterator.next());
+        }
+        System.out.println("-----------------------------");
+
+        // for each loop example
+        System.out.println("Using for each loop:");
+        for (String city : newCitiesSet) {
+            System.out.println("next city: " + city);
+        }
+        System.out.println("-----------------------------");
+
+        //toArray() - converts a set into an array
+        // advance implementation with reference operator
+        String[] citiesArray = newCitiesSet.toArray(String[]::new);
+        //String[] citiesArray = newCitiesSet.toArray(new String[newCitiesSet.size()]);
+        for(String city : citiesArray) {
+            System.out.println("Conversion to Array: " + city);
+        }
+        System.out.println("-----------------------------");
+
+        //removeAll() - removes all the elements from the set
+        System.out.println("Before removal - Cities List: " + cities);
+        System.out.println("Before removal - New Cities List: " + newCitiesSet);
+        System.out.println("removed all elements of cities list from newCitiesList: " + newCitiesSet.removeAll(cities));
+        System.out.println("After removal - Remaining New Cities List: " + newCitiesSet);
+        System.out.println("-----------------------------");
+
+        //clear() - removes all the elements from the set (more efficient than removeAll())
+        System.out.println("Size of newCitiesList before clear(): " + newCitiesSet.size());
+        newCitiesSet.clear();
+        System.out.println("Size of newCitiesList after clear(): " + newCitiesSet.size());
 
         //iterator() - returns an iterator that can be used to access elements of the set sequentially
         //remove() - removes the specified element from the set
